f0407790d43e17ba9c600c2dddc99bc7d4b90a92
minatorak <hotmail22866@gmail.com>
2018-05-12 15:50:59 +0700

    create SeconPage and add to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {ButtonListPage} from "../pages/button-list/button-list";
 import {ActionSheetComponent} from "../components/action-sheet/action-sheet";
-import { CustomComponent } from "../components/custom/custom";
+import {CustomComponent} from "../components/custom/custom";
+import {SecondPage} from "../pages/second/second";
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { CustomComponent } from "../components/custom/custom";
     MyApp,
     HomePage,
     ListPage,
-    ButtonListPage
+    ButtonListPage,
+    SecondPage
   ],
   providers: [
     StatusBar,
@@ -38,4 +40,5 @@ import { CustomComponent } from "../components/custom/custom";
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
